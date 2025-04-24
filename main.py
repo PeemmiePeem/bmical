2dfc0511af1adbf09d555f9ac28d09a915ce350d
Peem <peemxmanuelz@gmail.com>
2025-04-24 12:38:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ st.markdown("---")
 Weight=st.number_input("นํ้าหนัก (Kg) :",value=20,min_value=10,max_value=200,step=1,)
 Tall=st.number_input("ส่วนสูง (cm) :",value=100,min_value=1,max_value=200,step=1,)
 
+bg="""
+<style>
+.stApp {
+background-image: url ("https://wallpapercave.com/wp/wp9630625.jpg");
+}
+</style>
+"""
+st.html(bg)
+
 Final=Weight/(Tall/100)**2
 
 if st.button("Calculate"):
