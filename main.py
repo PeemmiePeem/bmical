fc404875ac71d247832d00a4d1ffa3e4dea64a47
Peem <peemxmanuelz@gmail.com>
2025-04-25 11:16:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ bg="""
 """
 #st.markdown(bg, unsafe_allow_html=True)
 st.html(bg)
-st.title("PEEM Bmi Calculator")
+st.title("Bmi Calculator")
 st.markdown("---")
 Weight=st.number_input("นํ้าหนัก (Kg) :",value=20,min_value=10,max_value=200,step=1,)
 Tall=st.number_input("ส่วนสูง (cm) :",value=100,min_value=1,max_value=200,step=1,)
