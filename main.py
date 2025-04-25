1579f4d250a7d42696e1e5659ff89bd17c5eb24a
Peem <peemxmanuelz@gmail.com>
2025-04-25 12:25:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,50 +20,50 @@ if st.button("Calculate"):
     if Sex=="ชาย":
         if Sex == "ชาย" and Final < 19:
             st.info(f"{Final:.2f} น้อยกว่า 19 ")
-            st.success(f"{Final} นํ้าหนักตํ่ากว่าเกณฑ์ ")
+            st.success(f"{Final:.2f} นํ้าหนักตํ่ากว่าเกณฑ์ ")
             st.image("Man_1.jpg")
         
         elif Sex == "ชาย" and Final >=19.0 and Final <=24.9:
             st.info (f"{Final:.2f} อยู่ระหว่าง 19.0 - 24.9")
-            st.success(f"{Final} นํ้าหนักสมส่วน")
+            st.success(f"{Final:.2f} นํ้าหนักสมส่วน")
             st.warning(f"ภาวะแทรกซ้อน : ภาวะนํ้าหนักเกินระยะเริ่มต้น")
             st.image("Man_2.jpg")
 
         elif Sex == "ชาย" and Final >=25 and Final <=29.9:
             st.info(f"{Final:.2f} อยู่ระหว่าง 25.0 - 29.9")
-            st.success(f"{Final} นํ้าหนักอยู่ในเกณฑ์อ้วน")
+            st.success(f"{Final:.2f} นํ้าหนักอยู่ในเกณฑ์อ้วน")
             st.warning(f"ภาวะแทรกซ้อน : ภาวะนํ็านหนักเกินมากระยะอ้วนเริ่มต้น")
             st.image("Man_3.jpg")
 
         else :
             if Final >=30.0:
                 st.error(f"{Final:.2f} มากกว่า 30")
-                st.success(f"{Final} นํ้าหนักอยู่ในเกณฑ์อ้วนมาก ")
+                st.success(f"{Final:.2f} นํ้าหนักอยู่ในเกณฑ์อ้วนมาก ")
                 st.warning(f"ภาวะแทรกซ้อน : ภาวะนํ้าหนักเกินมากโรคอ้วน")
                 st.image("Man_4.jpg")
         
     else:
         if Final < 18:
             st.info(f"{Final:.2f} น้อยกว่า 18 ")
-            st.success(f"{Final} นํ้าหนักตํ่ากว่าเกณฑ์ ")
+            st.success(f"{Final:.2f} นํ้าหนักตํ่ากว่าเกณฑ์ ")
             st.image("Woman_1.jpg")
 
         elif Final >=18.0 and Final <=23.9:
             st.info (f"{Final:.2f} อยู่ระหว่าง 18.0 - 23.9")
-            st.success(f"{Final} นํ้าหนักสมส่วน")
+            st.success(f"{Final:.2f} นํ้าหนักสมส่วน")
             st.warning(f"ภาวะแทรกซ้อน : ภาวะนํ้าหนักเกินระยะเริ่มต้น")
             st.image("Woman_2.jpg")
 
         elif Final >=24 and Final <=29.9:
             st.info(f"{Final:.2f} อยู่ระหว่าง 24.0 - 29.9")
-            st.success(f"{Final} นํ้าหนักอยู่ในเกณฑ์อ้วน")
+            st.success(f"{Final:.2f} นํ้าหนักอยู่ในเกณฑ์อ้วน")
             st.warning(f"ภาวะแทรกซ้อน : ภาวะนํ็านหนักเกินมากระยะอ้วนเริ่มต้น")
             st.image("Woman_3.jpg")
         
         else :
             if Final >=30.0:
                 st.error(f"{Final:.2f} มากกว่า 30")
-                st.success(f"{Final} นํ้าหนักอยู่ในเกณฑ์อ้วนมาก ")
+                st.success(f"{Final:.2f} นํ้าหนักอยู่ในเกณฑ์อ้วนมาก ")
                 st.warning(f"ภาวะแทรกซ้อน : ภาวะนํ้าหนักเกินมากโรคอ้วน")
                 st.image("Woman_4.jpg")
 
