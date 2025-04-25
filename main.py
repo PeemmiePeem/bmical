a931f2aa16c262e77cbe4710535251813f80ae3b
Peem <peemxmanuelz@gmail.com>
2025-04-25 15:35:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bg="""
     background-position: center;
     background-repeat: no-repeat;
     background-attachment: fixed;
-    background-color: #121212;
+    background-color: #FF6F61;
     color : #FFFFF0;
 }
 </style>
